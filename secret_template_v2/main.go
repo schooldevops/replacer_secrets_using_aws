3d04d270c6840487ab25264843e8c0c1bf49f44b
kido <baekido@gmail.com>
2021-06-30 11:08:17 +0900

    add logging for CI Lib

--- secret_template_v2/main.go
+++ secret_template_v2/main.go
@@ -19,43 +19,115 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+// DefaultRegion is default aws region
+const DefaultRegion = "ap-northeast-2"
+
+// DefaultAwsCredentialPath is your aws credential path
+const DefaultAwsCredentialPath = "~/.aws"
+
+// DefaultSecretName is default SecretName
+const DefaultSecretName = ""
+
+// DefaultProfile is default profile
+const DefaultProfile = "default"
+
 //	SecretConfig 시크릿 설정 구조체
 type SecretConfig struct {
-	Profile    string            `yaml:"profile"`
-	Region     string            `yaml:"region"`
-	Secrets    string            `yaml:"secrets"`
-	SecretKeys map[string]string `yaml:"secretkeys,omitempty"`
+	Profile      string            `yaml:"profile"`
+	Ext          string            `yaml:"ext"`
+	TargetPath   string            `yaml:"targetPath"`
+	Region       string            `yaml:"region"`
+	Secrets      string            `yaml:"secrets"`
+	Environments []string          `yaml:"environments"`
+	SecretKeys   map[string]string `yaml:"secretkeys,omitempty"`
 }
 
 var secretConfig = SecretConfig{}
 
+var myLogger *log.Logger
+
 func main() {
-	yamlFile := readFile("secretConfig.yml")
-	err := yaml.Unmarshal([]byte(yamlFile), &secretConfig)
+	// 로그파일 오픈
+	fpLog, err := os.OpenFile("logfile.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
-		log.Fatalf("Unmarshal: %v", err)
+		panic(err)
 	}
+	defer fpLog.Close()
+	myLogger = log.New(fpLog, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+	myLogger.Println("------- Start Replacing Secrets. -------")
+	yamlFile := readFile("secretConfig.yml")
+	err = yaml.Unmarshal([]byte(yamlFile), &secretConfig)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v\n", err)
+	}
+
+	myLogger.Println("INFO Read secretConfig.yml")
+
+	//	환경 변수를 돌면서, 값을 조회하고 처리한다.
+	for _, value := range secretConfig.Environments {
+		err, targetFile := makeTargetFile(value, secretConfig.Ext, secretConfig.TargetPath)
+		myLogger.Printf("INFO MakeTargetFile [%s, %s %s] \n", secretConfig.TargetPath, value, secretConfig.Ext, err)
+		if err == nil {
+			result := replaceConfigFiles(&secretConfig, targetFile)
+			myLogger.Println("INFO Replace result is ", result)
+		} else {
+			myLogger.Fatalf("ERROR File is not exists [%s, %s]\n", value, secretConfig.Ext)
+		}
+	}
+
+	myLogger.Println("------- Done Replacing Secrets. -------")
+}
+
+func makeTargetFile(environment string, ext string, targetPath string) (error, string) {
+	var targetFile string
+
+	prefix := "application"
+	// fmt.Println("env: ", environment, " ext: ", ext)
+	if environment == "default" {
+		targetFile = fmt.Sprintf("%s%s.%s", targetPath, prefix, ext)
+	} else {
+		targetFile = fmt.Sprintf("%s%s-%s.%s", targetPath, prefix, environment, ext)
+	}
+
+	if err := Exists(targetFile); err == nil {
+		return nil, targetFile
+	} else {
+		return err, ""
+	}
+}
+
+func Exists(name string) error {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return err
+		}
+	}
+	return nil
+}
+
+func replaceConfigFiles(secretConfig *SecretConfig, targetFile string) bool {
+	myLogger.Println("INFO Process targetFile: ", targetFile)
 
 	err, keyValueMap := getSecret()
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	log.Println(keyValueMap)
+	myLogger.Println("INFO Success reading secrets from AWS SecretsManager.")
 
 	mappedSecretMap := keyMapping(keyValueMap, secretConfig.SecretKeys)
-	// confFile := readFile("application.yml")
-	// log.Println(makingTemplate(string(confFile), mappedSecretMap))
+	myLogger.Println("INFO Parsed SecretMap by SecretKeys [%v]", secretConfig.SecretKeys)
 
-	resultByte := makingTemplate("application.yml", mappedSecretMap)
+	resultByte := makingTemplate(targetFile, mappedSecretMap)
 	log.Println("result \n", resultByte.String())
-
+	myLogger.Println("INFO Done replacing config file what is %s", targetFile)
+	return true
 }
 
 func readFile(filename string) string {
 	confFile, err := ioutil.ReadFile(filename)
 	if err != nil {
-		log.Printf("secretConfig read err   #%v ", err)
+		myLogger.Fatalf("ERROR secretConfig read err   #%v \n", err)
 	}
 
 	return string(confFile)
@@ -67,7 +139,7 @@ func keyMapping(secretMap map[string]interface{}, configMap map[string]string) m
 	for key, value := range configMap {
 		keyValueMap[key] = secretMap[value]
 
-		fmt.Println(key, value)
+		// fmt.Println(key, value)
 	}
 
 	return keyValueMap
@@ -111,6 +183,7 @@ func makingTemplate(a string, b map[string]interface{}) bytes.Buffer {
 // getSecret() is get secret from aws secretManager
 func getSecret() (error, map[string]interface{}) {
 
+	myLogger.Println("INFO Load Secrets from AWS SecretsManager.")
 	//Create a Secrets Manager client
 	svc := secretsmanager.New(session.New(),
 		aws.NewConfig().WithRegion(secretConfig.Region))
